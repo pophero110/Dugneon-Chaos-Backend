860e07a1421ecd1e296c9d36770481321f4350bd
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-01 17:49:47 -0500

    feat(cors): Add cors config

--- src/main/java/com/dungeonchaos/dungeonchaos/DungeonChaosApplication.java
+++ src/main/java/com/dungeonchaos/dungeonchaos/DungeonChaosApplication.java
@@ -1,9 +1,14 @@
 package com.dungeonchaos.dungeonchaos;
 
+import com.dungeonchaos.dungeonchaos.config.CorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
+@Import(CorsConfig.class)
 public class DungeonChaosApplication {
 
 	public static void main(String[] args) {
