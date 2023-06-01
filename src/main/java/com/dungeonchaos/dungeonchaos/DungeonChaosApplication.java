626c706ed642c6918500439a5a984735aa4970b3
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-01 09:58:17 -0500

    Initial commit

---
+++ src/main/java/com/dungeonchaos/dungeonchaos/DungeonChaosApplication.java
@@ -0,0 +1,13 @@
+package com.dungeonchaos.dungeonchaos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonChaosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonChaosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dungeonchaos/dungeonchaos/DungeonChaosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dungeonchaos.dungeonchaos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonChaosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
