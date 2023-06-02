fc4c4289af3844a1d2a91f3519cb30bd27a905fa
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-02 11:11:44 -0500

    feat(player): create player model and base entity class

---
+++ src/main/java/com/dungeonchaos/dungeonchaos/model/Player.java
@@ -0,0 +1,9 @@
+package com.dungeonchaos.dungeonchaos.model;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+
+@Entity
+@Table(name = "player")
+public class Player extends BaseEntity {
+}
