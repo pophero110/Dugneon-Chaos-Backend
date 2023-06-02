1c1ec0e6d1ccec9aaf98924b34875fd145658177
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-02 14:38:33 -0500

    feat(player): create player constructor method
    1. create player by character object

--- src/main/java/com/dungeonchaos/dungeonchaos/model/Player.java
+++ src/main/java/com/dungeonchaos/dungeonchaos/model/Player.java
@@ -6,4 +6,17 @@ import javax.persistence.Entity;
 @Entity
 @Table(name = "player")
 public class Player extends BaseEntity {
+
+
+    public Player(Character character) {
+        this.setName(character.getName());
+        this.setAttack(character.getAttack());
+        this.setHealth(character.getHealth());
+        this.setDefense(character.getDefense());
+        this.setSpeed(character.getSpeed());
+    }
+
+    public Player() {
+
+    }
 }
