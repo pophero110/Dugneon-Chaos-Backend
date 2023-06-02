844167e69a00ced4c76eb944f0b6bcc030805704
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-02 11:12:29 -0500

    refactor(character): refactor code with inheritance

--- src/main/java/com/dungeonchaos/dungeonchaos/model/Character.java
+++ src/main/java/com/dungeonchaos/dungeonchaos/model/Character.java
@@ -1,63 +1,17 @@
 package com.dungeonchaos.dungeonchaos.model;
 
 import javax.persistence.*;
+import javax.persistence.Entity;
 
 @Entity
 @Table(name= "character")
-public class Character {
-    @Id
-    @Column
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column
-    private String name;
-
-    @Column
-    private int health;
-
-    @Column
-    private int attack;
-
-    @Column
-    private int defense;
-
-    @Column
-    private int speed;
+public class Character extends BaseEntity {
 
     public Character(String name, int health, int attack, int defense, int speed) {
-        this.name = name;
-        this.health = health;
-        this.attack = attack;
-        this.defense = defense;
-        this.speed = speed;
+        super(name, health, attack, defense, speed);
     }
 
     public Character() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public int getAttack() {
-        return attack;
-    }
-
-    public int getDefense() {
-        return defense;
-    }
-
-    public int getSpeed() {
-        return speed;
+        super();
     }
 }
