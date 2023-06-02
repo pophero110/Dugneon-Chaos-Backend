056ac4053346f197dc2675f5d18697670bb8e2e6
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-01 19:54:49 -0500

    refactor(board): remove unused code

--- src/main/java/com/dungeonchaos/dungeonchaos/controller/BoardController.java
+++ src/main/java/com/dungeonchaos/dungeonchaos/controller/BoardController.java
@@ -1,14 +1,10 @@
 package com.dungeonchaos.dungeonchaos.controller;
 
-
 import com.dungeonchaos.dungeonchaos.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 
 @RestController
