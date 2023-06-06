b479f35b8cb1ce42e6f9f2e95b66f9505ab1a0bf
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-05 22:26:21 -0500

    feat(seed): add more seed data

--- src/main/java/com/dungeonchaos/dungeonchaos/seed/SeedRunner.java
+++ src/main/java/com/dungeonchaos/dungeonchaos/seed/SeedRunner.java
@@ -2,8 +2,13 @@ package com.dungeonchaos.dungeonchaos.seed;
 
 import com.dungeonchaos.dungeonchaos.model.Board;
 import com.dungeonchaos.dungeonchaos.model.Character;
+import com.dungeonchaos.dungeonchaos.model.Monster;
+import com.dungeonchaos.dungeonchaos.model.Player;
 import com.dungeonchaos.dungeonchaos.repository.BoardRepository;
 import com.dungeonchaos.dungeonchaos.repository.CharacterRepository;
+import com.dungeonchaos.dungeonchaos.repository.MonsterRepository;
+import com.dungeonchaos.dungeonchaos.repository.PlayerRepository;
+import com.dungeonchaos.dungeonchaos.service.FightService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,16 +22,26 @@ public class SeedRunner implements ApplicationRunner {
 
     private final BoardRepository boardRepository;
 
+    private final MonsterRepository monsterRepository;
+    private final PlayerRepository playerRepository;
+
+    private final FightService fightService;
+
 
     @Autowired
-    SeedRunner(CharacterRepository characterRepository, BoardRepository boardRepository) {
+    SeedRunner(CharacterRepository characterRepository, BoardRepository boardRepository, MonsterRepository monsterRepository, PlayerRepository playerRepository, FightService fightService) {
         this.characterRepository = characterRepository;
         this.boardRepository = boardRepository;
+        this.monsterRepository = monsterRepository;
+        this.playerRepository = playerRepository;
+        this.fightService = fightService;
     }
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        createCharacters();
+        createCharactersAndPlayer();
         createBoard();
+        createMonsters();
+        createFight();
     }
 
     private void createBoard() {
@@ -51,17 +66,36 @@ public class SeedRunner implements ApplicationRunner {
             Board board = new Board(jsonData);
             boardRepository.save(board);
         } catch (JsonProcessingException e) {
-            // Handle the exception
             e.printStackTrace();
-            return;
         }
 
     }
-    private void createCharacters() {
+    private void createCharactersAndPlayer() {
         Character warrior = new Character("Warrior", 90, 10, 20, 30);
-        Character rogue = new Character("Rogue", 80, 15, 10, 40);
-
+        Character rogue = new Character("Rogue", 80, 30, 10, 40);
+        Character slow = new Character("Slow Speed", 80, 15, 10, 0);
+        Character fast = new Character("Fast Speed", 80, 15, 10, 999);
+        Player playerWarrior = new Player(warrior);
+        Player playerRogue = new Player(rogue);
+        Player playerSlow = new Player(slow);
+        Player playerFast = new Player(fast);
+        playerRepository.save(playerWarrior);
+        playerRepository.save(playerRogue);
+        playerRepository.save(playerSlow);
+        playerRepository.save(playerFast);
         characterRepository.save(warrior);
         characterRepository.save(rogue);
+        characterRepository.save(slow);
+        characterRepository.save(fast);
+    }
+
+    private void createMonsters() {
+        Monster goblin = new Monster("Goblin", 30, 5,0,20);
+
+        monsterRepository.save(goblin);
+    }
+
+    private void createFight() {
+        fightService.startFight(1L, 1L);
     }
 }
