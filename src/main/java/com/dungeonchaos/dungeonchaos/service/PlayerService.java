0d714806e6b3f5bed0b18c077b2522c56d2c9d98
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-07 13:39:47 -0500

    feat(player): create inventory by default when create a player

--- src/main/java/com/dungeonchaos/dungeonchaos/service/PlayerService.java
+++ src/main/java/com/dungeonchaos/dungeonchaos/service/PlayerService.java
@@ -2,8 +2,10 @@ package com.dungeonchaos.dungeonchaos.service;
 
 import com.dungeonchaos.dungeonchaos.exception.InformationNotFoundException;
 import com.dungeonchaos.dungeonchaos.model.Character;
+import com.dungeonchaos.dungeonchaos.model.Inventory;
 import com.dungeonchaos.dungeonchaos.model.Player;
 import com.dungeonchaos.dungeonchaos.repository.CharacterRepository;
+import com.dungeonchaos.dungeonchaos.repository.InventoryRepository;
 import com.dungeonchaos.dungeonchaos.repository.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,11 +16,13 @@ import java.util.Optional;
 public class PlayerService {
     private PlayerRepository playerRepository;
     private CharacterRepository characterRepository;
+    private InventoryRepository inventoryRepository;
 
     @Autowired
-    PlayerService(PlayerRepository playerRepository, CharacterRepository characterRepository) {
+    PlayerService(PlayerRepository playerRepository, CharacterRepository characterRepository, InventoryRepository inventoryRepository) {
         this.playerRepository = playerRepository;
         this.characterRepository = characterRepository;
+        this.inventoryRepository = inventoryRepository;
     }
 
     public Player createPlayer(Long selectedCharacterId) {
@@ -26,7 +30,15 @@ public class PlayerService {
                 () -> new InformationNotFoundException("Character is not found with id: " + selectedCharacterId)
         );
         Player createdPlayer = new Player(character);
-         playerRepository.save(createdPlayer);
+        Inventory inventory = new Inventory();
+        createdPlayer.setInventory(inventory);
+        inventory.setPlayer(createdPlayer);
+        playerRepository.save(createdPlayer);
         return createdPlayer;
+    }
+
+    public Player getPlayer(Long playerId) {
+        Player player = this.playerRepository.findById(playerId).orElseThrow(() -> new InformationNotFoundException("Player is not found with id " + playerId));
+        return player;
     }
 }
