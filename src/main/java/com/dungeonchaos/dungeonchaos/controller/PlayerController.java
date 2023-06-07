bf31ab10e38c4744d1bddf2e3ce3bc51c89f1af7
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-07 13:43:24 -0500

    feat(player): create getPlayer endpoint for testing

--- src/main/java/com/dungeonchaos/dungeonchaos/controller/PlayerController.java
+++ src/main/java/com/dungeonchaos/dungeonchaos/controller/PlayerController.java
@@ -6,10 +6,7 @@ import com.dungeonchaos.dungeonchaos.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 import java.util.Optional;
@@ -26,7 +23,14 @@ public class PlayerController {
     }
     @PostMapping(path = "")
     public ResponseEntity<Player> createPlayer(@RequestBody Map<String, String> body) {
+        //TODO: refactor using request body class
         Player createdPlayer = playerService.createPlayer(Long.parseLong(body.get("selectedCharacterId")));
+        return new ResponseEntity<>(createdPlayer, HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/{playerId}")
+    public ResponseEntity<Player> getPlayer(@PathVariable Long playerId) {
+        Player createdPlayer = playerService.getPlayer(playerId);
         return new ResponseEntity<>(createdPlayer, HttpStatus.CREATED);
     }
 }
