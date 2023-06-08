53b5be2224661d9c9a81848e77c3e2ca9af80ac8
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-07 22:05:52 -0500

    feat(reward): create reward controller

---
+++ src/main/java/com/dungeonchaos/dungeonchaos/controller/RewardController.java
@@ -0,0 +1,33 @@
+package com.dungeonchaos.dungeonchaos.controller;
+
+
+import com.dungeonchaos.dungeonchaos.model.Reward.Reward;
+import com.dungeonchaos.dungeonchaos.model.Reward.RewardType;
+import com.dungeonchaos.dungeonchaos.request.RewardRequest;
+import com.dungeonchaos.dungeonchaos.service.RewardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(path= "/api/rewards")
+public class RewardController {
+
+    private RewardService rewardService;
+
+    @Autowired
+    RewardController(RewardService rewardService) {
+        this.rewardService = rewardService;
+    }
+
+    @PostMapping(path = "")
+    public ResponseEntity<Reward> createReward(@RequestBody RewardRequest rewardRequest) {
+        Reward reward = rewardService.createReward(RewardType.ITEM, rewardRequest.getPlayerId());
+        return new ResponseEntity<>(reward, HttpStatus.CREATED);
+    }
+}
