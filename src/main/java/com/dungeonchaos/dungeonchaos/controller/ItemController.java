6f3f39dcc2809f5264fa82111796f5a6d4792c07
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-07 22:08:33 -0500

    feat(item): create item controller

---
+++ src/main/java/com/dungeonchaos/dungeonchaos/controller/ItemController.java
@@ -0,0 +1,33 @@
+package com.dungeonchaos.dungeonchaos.controller;
+
+
+import com.dungeonchaos.dungeonchaos.model.Item.Item;
+import com.dungeonchaos.dungeonchaos.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/items")
+public class ItemController {
+
+
+    private ItemService itemService;
+
+    @Autowired
+    ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Item>> getItems() {
+        List<Item> items= itemService.getItems();
+        return new ResponseEntity<>(items, HttpStatus.OK);
+    }
+}
