5b9aba722d306e1d2cd2c937e100d59eafe507b7
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-09 15:00:41 -0500

    Rebuild

--- src/main/java/com/dungeonchaos/dungeonchaos/DungeonChaosApplication.java
+++ src/main/java/com/dungeonchaos/dungeonchaos/DungeonChaosApplication.java
@@ -12,11 +12,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class DungeonChaosApplication {
 
 	public static void main(String[] args) {
-
-
-		// Set the default profile as 'dev' to be used if the 'SPRING_PROFILES_ACTIVE' environment variable is not set
-		System.setProperty("spring.profiles.default", "dev");
-
 		SpringApplication.run(DungeonChaosApplication.class, args);
 	}
 
